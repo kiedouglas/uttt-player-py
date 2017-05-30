77dbe51d6b0f3609f3e2f482e2663a852a4dd099
Bharat Reddy <me@bharatreddy.com>
2017-05-30 21:25:05 +0100

    Added author email

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from setuptools import setup
 setup(name='ultimate_ttt_player',
       version='0.1',
       description='Sample player implementations for UTTT games',
-      author='socialgorithm'
+      author='socialgorithm',
+      author_email='hellp@socialgorithm.org',
       url='https://github.com/socialgorithm/uttt-player-py',
       packages=['ultimate_ttt_player'],
 	  setup_requires=['ultimate_ttt','pytest-runner'],
